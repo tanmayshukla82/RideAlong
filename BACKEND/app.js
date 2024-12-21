1e0ce737fe4b617633c4744ec300c2efd192fb52
Tanmay Shukla <tanmayshukla82@gmail.com>
2024-12-21 18:20:27 +0530

    set up
    set up

---
+++ BACKEND/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+export default app;
---
+++ BACKEND/server.js
@@ -0,0 +1,8 @@
+import http from 'http';
+import app from './app.js';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(PORT, ()=>{
+    console.log(`connected to PORT : ${PORT}`);
+});
